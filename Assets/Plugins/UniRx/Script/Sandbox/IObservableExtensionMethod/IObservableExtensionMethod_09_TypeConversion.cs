9dba0c746901f2fe2babacdc76f7b458b790a12d
ToshikiHata <toshiki_hata@f4samurai.jp>
2023-01-01 17:37:58 +0900

    IObservableの拡張メソッドについて勉強: 型変換

---
+++ Assets/Plugins/UniRx/Script/Sandbox/IObservableExtensionMethod/IObservableExtensionMethod_09_TypeConversion.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UniRx;
+using UnityEngine;
+
+namespace Sandbox.IObservableExtensionMethod
+{
+    public class IObservableExtensionMethod_09_TypeConversion : MonoBehaviour
+    {
+        [ContextMenu(nameof(Cast))]
+        void Cast()
+        {
+            // Castは名前の通りで､指定された型にキャスト. 失敗した場合はOnErrorを流す.
+            var source = new Subject<object>();
+            var subscription = source
+                .Cast<object, int>()
+                .Subscribe(
+                    i => UnityEngine.Debug.Log($"OnNext({i})"),
+                    ex => UnityEngine.Debug.Log($"OnException({ex})"),
+                    () => UnityEngine.Debug.Log($"OnCompleted()")
+                );
+
+            source.OnNext(0);
+            source.OnNext(1);
+            source.OnNext(2);
+            source.OnNext("string");
+            source.OnNext(3);
+
+            subscription.Dispose();
+        }
+
+        [ContextMenu(nameof(OfType))]
+        void OfType()
+        {
+            // OfTypeはCastと同じく型変換を行うが､型変換に失敗した場合はなにもしないのが違い
+            var source = new Subject<object>();
+            var subscription = source
+                .OfType<object, int>()
+                .Subscribe(
+                    i => UnityEngine.Debug.Log($"OnNext({i})"),
+                    ex => UnityEngine.Debug.Log($"OnException({ex})"),
+                    () => UnityEngine.Debug.Log($"OnCompleted()")
+                );
+
+            source.OnNext(0);
+            source.OnNext(1);
+            source.OnNext(2);
+            source.OnNext("string");
+            source.OnNext(3);
+
+            subscription.Dispose();
+        }
+    }
+}
